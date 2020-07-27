b9af21b6db5803e3ef7aeda1f3ac39269ae0a59e
Alex <ancorp@gmail.com>
2020-07-27 17:52:56 +0700

    release

--- vis_test.py
+++ vis_test.py
@@ -1,38 +1,10 @@
 import numpy as np
 
 
-def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 0
-    while True:
-        count += 1
-        predict = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict:
-            return (count)  # выход из цикла, если угадали
-
-
-
-def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 1
-
-    predict = np.random.randint(1,101)
-    while number != predict:
-        count+=1
-        if number > predict:
-            predict += 1
-        elif number < predict:
-            predict -= 1
-
-    return(count)
-
-
 def game_core_v3(number):
     count = 1
 
-    predict = 50
+    predict = 50  # Начинаем гадать с 50
     number_from = 1
     number_to = 101
 
@@ -41,16 +13,17 @@ def game_core_v3(number):
 
         if predict < number:
             number_from = predict
-        elif number < predict:
+        elif predict > number:
             number_to = predict
 
+        # После каждой попытки сужаем границы генерации случайного числа
         predict = np.random.randint(number_from, number_to)
     else:
         return count
 
 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    # Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
@@ -58,6 +31,7 @@ def score_game(game_core):
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return (score)
+    return score
+
 
 score_game(game_core_v3)
